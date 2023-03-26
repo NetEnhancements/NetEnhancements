eab39bfe509c86d169da05c3d64e052f045bc5b3
Maikel <skami.creations@gmail.com>
2023-03-26 20:11:26 +0200

    Added DateExtensions

---
+++ src/NetEnhancements.Util/DateExtensions.cs
@@ -0,0 +1,183 @@
+﻿namespace NetEnhancements.Util
+{
+    /// <summary>
+    /// Provides extension methods for DateTime and TimeSpan objects.
+    /// </summary>
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Rounds a DateTime object to the nearest TimeSpan.
+        /// </summary>
+        /// <param name="dt">The DateTime object to round.</param>
+        /// <param name="ts">The TimeSpan to round to.</param>
+        /// <returns>A new DateTime object rounded to the nearest TimeSpan.</returns>
+        public static DateTime RoundToNearest(this DateTime dt, TimeSpan ts)
+        {
+            var delta = dt.Ticks % ts.Ticks;
+            var roundUp = delta > ts.Ticks / 2;
+            var offset = roundUp ? ts.Ticks : 0;
+
+            return new DateTime(dt.Ticks + offset - delta, dt.Kind);
+        }
+
+        /// <summary>
+        /// Rounds a TimeSpan object to the nearest TimeSpan.
+        /// </summary>
+        /// <param name="ts">The TimeSpan object to round.</param>
+        /// <param name="roundTo">The TimeSpan to round to.</param>
+        /// <returns>A new TimeSpan object rounded to the nearest TimeSpan.</returns>
+        public static TimeSpan RoundToNearest(this TimeSpan ts, TimeSpan roundTo)
+        {
+            var ticks = (long)(Math.Round(ts.Ticks / (double)roundTo.Ticks) * roundTo.Ticks);
+            return new TimeSpan(ticks);
+        }
+
+        /// <summary>
+        /// Determines if a DateTime object falls between two specified DateTime objects.
+        /// </summary>
+        /// <param name="dt">The DateTime object to check.</param>
+        /// <param name="start">The start DateTime object to compare with.</param>
+        /// <param name="end">The end DateTime object to compare with.</param>
+        /// <returns>True if the DateTime object is between the start and end dates, inclusive; otherwise, false.</returns>
+        public static bool IsBetweenTwoDates(this DateTime dt, DateTime start, DateTime end)
+        {
+            return dt >= start && dt <= end;
+        }
+
+        /// <summary>
+        /// Determines if two date ranges overlap.
+        /// </summary>
+        /// <param name="aStart">The start DateTime of the first date range.</param>
+        /// <param name="aEnd">The end DateTime of the first date range.</param>
+        /// <param name="bStart">The start DateTime of the second date range.</param>
+        /// <param name="bEnd">The end DateTime of the second date range.</param>
+        /// <returns>True if the date ranges overlap; otherwise, false.</returns>
+        public static bool DoDatesOverlap(DateTime aStart, DateTime aEnd, DateTime bStart, DateTime bEnd)
+        {
+            return aStart < bEnd && bStart < aEnd;
+        }
+
+        /// <summary>
+        /// Converts a Unix time to a DateTime object.
+        /// </summary>
+        /// <param name="unixTime">The Unix time to convert, in seconds since the Unix epoch (1970-01-01 00:00:00 UTC).</param>
+        /// <returns>A DateTime object representing the converted Unix time.</returns>
+        public static DateTime FromUnixTime(in long unixTime)
+        {
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTime).ToLocalTime();
+            return dtDateTime;
+        }
+
+        /// <summary>
+        /// Converts a DateTime object to Unix time.
+        /// </summary>
+        /// <param name="dt">The DateTime object to convert.</param>
+        /// <returns>A long value representing the Unix time.</returns>
+        public static long ToUnixTime(this DateTime dt)
+        {
+            if (dt == DateTime.MinValue)
+            {
+                return 0;
+            }
+            var utcNow = new DateTimeOffset(dt);
+            return utcNow.ToUnixTimeSeconds();
+        }
+
+        /// <summary>
+        /// Generates an enumerable sequence of DateTime objects, one for each day in the specified range.
+        /// </summary>
+        /// <param name="from">The start date of the range, inclusive.</param>
+        /// <param name="thru">The end date of the range, inclusive.</param>
+        /// <returns>An IEnumerable&lt;DateTime&gt; containing one DateTime object for each day in the specified range.</returns>
+        public static IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+            {
+                yield return day;
+            }
+        }
+        
+        /// <summary>
+        /// Gets the start of the previous month for a given DateTime.
+        /// </summary>
+        /// <param name="dt">The DateTime object to get the start of the previous month from.</param>
+        /// <returns>A new DateTime object representing the start of the previous month.</returns>
+        public static DateTime StartOfPreviousMonth(this DateTime dt)
+        {
+            var previousMonth = dt.AddMonths(-1);
+            return new DateTime(previousMonth.Year, previousMonth.Month, 1);
+        }
+
+        /// <summary>
+        /// Gets the end of the previous month for a given DateTime.
+        /// </summary>
+        /// <param name="dt">The DateTime object to get the end of the previous month from.</param>
+        /// <returns>A new DateTime object representing the end of the previous month.</returns>
+        public static DateTime EndOfPreviousMonth(this DateTime dt)
+        {
+            return dt.StartOfPreviousMonth().AddMonths(1).AddTicks(-1);
+        }
+
+        /// <summary>
+        /// Gets the first occurrence of a specified day of the week in a given month.
+        /// </summary>
+        /// <param name="date">The DateTime object representing the month to find the first occurrence of the day of the week.</param>
+        /// <param name="firstDayOfWeek">The DayOfWeek value to find the first occurrence of in the month.</param>
+        /// <returns>A new DateTime object representing the first occurrence of the specified day of the week in the given month.</returns>
+        public static DateTime GetFirstDayOfMonth(this DateTime date, DayOfWeek firstDayOfWeek)
+        {
+            var currentMonth = new DateTime(date.Year, date.Month, 1);
+
+            while (currentMonth.DayOfWeek != firstDayOfWeek)
+            {
+                currentMonth = currentMonth.AddDays(1);
+            }
+
+            return currentMonth;
+        }
+
+        /// <summary>
+        /// Converts a TimeSpan object into a human-readable string representation.
+        /// </summary>
+        /// <param name="ts">The TimeSpan object to humanize.</param>
+        /// <returns>A string representing the human-readable version of the TimeSpan object.</returns>
+        public static string Humanize(this TimeSpan ts)
+        {
+            var formatString = string.Empty;
+
+            if (ts.Days > 0)
+            {
+                formatString += "d\\d\\ ";
+            }
+
+            if (ts.Hours > 0)
+            {
+                formatString += "h\\u\\ ";
+            }
+
+            if (ts.Minutes > 0)
+            {
+                formatString += "m\\m\\ ";
+            }
+
+            if (ts.Seconds > 0)
+            {
+                formatString += "s\\s\\ ";
+            }
+
+            if (ts.Milliseconds > 0)
+            {
+                formatString += "fff\\m\\s";
+            }
+
+            var result = ts.ToString(formatString).Trim().Replace(" 0", " ").Replace(" 0", " ");
+            if (result.StartsWith('0'))
+            {
+                result = result[1..];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/NetEnhancements.Util.Tests/DateExtensionsTests.cs
@@ -0,0 +1,591 @@
+﻿namespace NetEnhancements.Util.Tests
+{
+    public class DateExtensionsTests
+    {
+        [Test]
+        public void RoundToNearest_Goes_Up()
+        {
+            // Arrange
+            var dt = new DateTime(2019, 5, 11, 8, 0, 35);
+            var expected = new DateTime(2019, 5, 11, 8, 1, 0);
+
+            // Act and Assert
+            Assert.AreEqual(expected, dt.RoundToNearest(TimeSpan.FromMinutes(1)));
+        }
+
+        [Test]
+        public void RoundToNearest_Increments_Hours()
+        {
+            // Arrange
+            var dt = new DateTime(2019, 5, 11, 7, 59, 35);
+            var expected = new DateTime(2019, 5, 11, 8, 0, 0);
+
+            // Act and Assert
+            Assert.AreEqual(expected, dt.RoundToNearest(TimeSpan.FromMinutes(1)));
+        }
+
+        [Test]
+        public void IsBetweenTwoDates_DateTimeIsBetween_ReturnsTrue()
+        {
+            // Arrange
+            var start = new DateTime(2022, 1, 1);
+            var end = new DateTime(2022, 12, 31);
+            var dt = new DateTime(2022, 6, 1);
+
+            // Act
+            var result = dt.IsBetweenTwoDates(start, end);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsBetweenTwoDates_DateTimeIsBeforeStart_ReturnsFalse()
+        {
+            // Arrange
+            var start = new DateTime(2022, 1, 1);
+            var end = new DateTime(2022, 12, 31);
+            var dt = new DateTime(2021, 12, 31);
+
+            // Act
+            var result = dt.IsBetweenTwoDates(start, end);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsBetweenTwoDates_DateTimeIsAfterEnd_ReturnsFalse()
+        {
+            // Arrange
+            var start = new DateTime(2022, 1, 1);
+            var end = new DateTime(2022, 12, 31);
+            var dt = new DateTime(2023, 1, 1);
+
+            // Act
+            var result = dt.IsBetweenTwoDates(start, end);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Humanize_ZeroTimeSpan_ReturnsEmptyString()
+        {
+            // Arrange
+            var ts = TimeSpan.Zero;
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("0:00:00", result);
+        }
+
+        [Test]
+        public void Humanize_OneDayTimeSpan_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(1, 0, 0, 0);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("1d", result);
+        }
+
+        [Test]
+        public void Humanize_OneHourTimeSpan_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(0, 1, 0, 0);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("1u", result);
+        }
+
+        [Test]
+        public void Humanize_OneMinuteTimeSpan_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(0, 0, 1, 0);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("1m", result);
+        }
+
+        [Test]
+        public void Humanize_OneSecondTimeSpan_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(0, 0, 0, 1);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("1s", result);
+        }
+
+        [Test]
+        public void Humanize_OneMillisecondTimeSpan_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(0, 0, 0, 0, 1);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("01ms", result);
+        }
+
+        [Test]
+        public void Humanize_MultipleTimeUnits_ReturnsFormattedString()
+        {
+            // Arrange
+            var ts = new TimeSpan(1, 2, 3, 4, 5);
+
+            // Act
+            var result = ts.Humanize();
+
+            // Assert
+            Assert.AreEqual("1d 2u 3m 4s 5ms", result);
+        }
+
+        [Test]
+        public void GetFirstDayOfMonth_ReturnsFirstMonday_WhenCalledWithMonday()
+        {
+            // Arrange
+            var date = new DateTime(2023, 3, 19); // a Sunday
+            var firstDayOfWeek = DayOfWeek.Monday;
+            var expectedFirstDay = new DateTime(2023, 3, 6); // the first Monday in March 2023
+
+            // Act
+            var result = date.GetFirstDayOfMonth(firstDayOfWeek);
+
+            // Assert
+            Assert.AreEqual(expectedFirstDay, result);
+        }
+
+        [Test]
+        public void GetFirstDayOfMonth_ReturnsFirstSunday_WhenCalledWithSunday()
+        {
+            // Arrange
+            var date = new DateTime(2023, 3, 19); // a Sunday
+            var firstDayOfWeek = DayOfWeek.Sunday;
+            var expectedFirstDay = new DateTime(2023, 3, 5); // the first Sunday in March 2023
+
+            // Act
+            var result = date.GetFirstDayOfMonth(firstDayOfWeek);
+
+            // Assert
+            Assert.AreEqual(expectedFirstDay, result);
+        }
+
+        [Test]
+        public void GetFirstDayOfMonth_ReturnsFirstTuesday_WhenCalledWithTuesday()
+        {
+            // Arrange
+            var date = new DateTime(2023, 3, 19); // a Sunday
+            var firstDayOfWeek = DayOfWeek.Tuesday;
+            var expectedFirstDay = new DateTime(2023, 3, 7); // the first Tuesday in March 2023
+
+            // Act
+            var result = date.GetFirstDayOfMonth(firstDayOfWeek);
+
+            // Assert
+            Assert.AreEqual(expectedFirstDay, result);
+        }
+
+        [Test]
+        public void DoDatesOverlap_WhenAEndIsBeforeBStart_ReturnsFalse()
+        {
+            // Arrange
+            var aStart = new DateTime(2023, 3, 1);
+            var aEnd = new DateTime(2023, 3, 10);
+            var bStart = new DateTime(2023, 3, 15);
+            var bEnd = new DateTime(2023, 3, 20);
+
+            // Act
+            var result = DateExtensions.DoDatesOverlap(aStart, aEnd, bStart, bEnd);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void DoDatesOverlap_WhenBEndIsBeforeAStart_ReturnsFalse()
+        {
+            // Arrange
+            var aStart = new DateTime(2023, 3, 15);
+            var aEnd = new DateTime(2023, 3, 20);
+            var bStart = new DateTime(2023, 3, 1);
+            var bEnd = new DateTime(2023, 3, 10);
+
+            // Act
+            var result = DateExtensions.DoDatesOverlap(aStart, aEnd, bStart, bEnd);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void DoDatesOverlap_WhenADateRangeIsContainedInBDateRange_ReturnsTrue()
+        {
+            // Arrange
+            var aStart = new DateTime(2023, 3, 5);
+            var aEnd = new DateTime(2023, 3, 10);
+            var bStart = new DateTime(2023, 3, 1);
+            var bEnd = new DateTime(2023, 3, 15);
+
+            // Act
+            var result = DateExtensions.DoDatesOverlap(aStart, aEnd, bStart, bEnd);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DoDatesOverlap_WhenBDateRangeIsContainedInADateRange_ReturnsTrue()
+        {
+            // Arrange
+            var aStart = new DateTime(2023, 3, 1);
+            var aEnd = new DateTime(2023, 3, 15);
+            var bStart = new DateTime(2023, 3, 5);
+            var bEnd = new DateTime(2023, 3, 10);
+
+            // Act
+            var result = DateExtensions.DoDatesOverlap(aStart, aEnd, bStart, bEnd);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DoDatesOverlap_WhenDateRangesOverlap_ReturnsTrue()
+        {
+            // Arrange
+            var aStart = new DateTime(2023, 3, 5);
+            var aEnd = new DateTime(2023, 3, 15);
+            var bStart = new DateTime(2023, 3, 10);
+            var bEnd = new DateTime(2023, 3, 20);
+
+            // Act
+            var result = DateExtensions.DoDatesOverlap(aStart, aEnd, bStart, bEnd);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void RoundToNearest_RoundsToNearestDay()
+        {
+            // Arrange
+            var input = new TimeSpan(0, 23, 59, 59, 999);
+            var roundTo = new TimeSpan(1, 0, 0, 0);
+
+            // Act
+            var result = input.RoundToNearest(roundTo);
+
+            // Assert
+            var expected = new TimeSpan(1, 0, 0, 0);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void RoundToNearest_RoundsToNearestHour()
+        {
+            // Arrange
+            var input = new TimeSpan(0, 59, 59);
+            var roundTo = new TimeSpan(0, 1, 0, 0);
+
+            // Act
+            var result = input.RoundToNearest(roundTo);
+
+            // Assert
+            var expected = new TimeSpan(1, 0, 0);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void RoundToNearest_RoundsToNearestMinute()
+        {
+            // Arrange
+            var input = new TimeSpan(0, 0, 29, 59);
+            var roundTo = new TimeSpan(0, 0, 1, 0);
+
+            // Act
+            var result = input.RoundToNearest(roundTo);
+
+            // Assert
+            var expected = new TimeSpan(0, 0, 30, 0);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void RoundToNearest_RoundsToNearestSecond()
+        {
+            // Arrange
+            var input = new TimeSpan(0, 0, 0, 29, 500);
+            var roundTo = new TimeSpan(0, 0, 0, 1);
+
+            // Act
+            var result = input.RoundToNearest(roundTo);
+
+            // Assert
+            var expected = new TimeSpan(0, 0, 30);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void RoundToNearest_RoundsToNearestSecond_WithNoMilliseconds()
+        {
+            // Arrange
+            var input = new TimeSpan(0, 0, 0, 29, 999);
+            var roundTo = new TimeSpan(0, 0, 0, 1);
+
+            // Act
+            var result = input.RoundToNearest(roundTo);
+
+            // Assert
+            var expected = new TimeSpan(0, 0, 30);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void EachDay_SameDay_ReturnsOneDay()
+        {
+            // Arrange
+            var from = new DateTime(2023, 3, 24);
+            var thru = new DateTime(2023, 3, 24);
+
+            // Act
+            var result = DateExtensions.EachDay(from, thru).ToList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result[0], Is.EqualTo(from));
+        }
+
+        [Test]
+        public void EachDay_OneDayApart_ReturnsTwoDays()
+        {
+            // Arrange
+            var from = new DateTime(2023, 3, 24);
+            var thru = new DateTime(2023, 3, 25);
+
+            // Act
+            var result = DateExtensions.EachDay(from, thru).ToList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo(from));
+            Assert.That(result[1], Is.EqualTo(thru));
+        }
+
+        [Test]
+        public void EachDay_MultipleDays_ReturnsExpectedDays()
+        {
+            // Arrange
+            var from = new DateTime(2023, 3, 24);
+            var thru = new DateTime(2023, 3, 27);
+
+            // Act
+            var result = DateExtensions.EachDay(from, thru).ToList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(4));
+            Assert.That(result[0], Is.EqualTo(new DateTime(2023, 3, 24)));
+            Assert.That(result[1], Is.EqualTo(new DateTime(2023, 3, 25)));
+            Assert.That(result[2], Is.EqualTo(new DateTime(2023, 3, 26)));
+            Assert.That(result[3], Is.EqualTo(new DateTime(2023, 3, 27)));
+        }
+
+        [Test]
+        public void EachDay_SameDay_ReturnsZeroDay()
+        {
+            // Arrange
+            var from = new DateTime(2023, 3, 25);
+            var thru = new DateTime(2023, 3, 24);
+
+            // Act
+            var result = DateExtensions.EachDay(from, thru).ToList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestFromUnixTime_Epoch()
+        {
+            // Arrange
+            long unixTime = 0;
+            DateTime expected = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+
+            // Act
+            DateTime actual = DateExtensions.FromUnixTime(unixTime);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestFromUnixTime_Positive()
+        {
+            // Arrange
+            long unixTime = 1679170800;
+            DateTime expected = new DateTime(2023, 3, 18, 20, 20, 0, DateTimeKind.Utc).ToLocalTime();
+
+            // Act
+            DateTime actual = DateExtensions.FromUnixTime(unixTime);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestFromUnixTime_Negative()
+        {
+            // Arrange
+            long unixTime = -2208988800; 
+            DateTime expected = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+            
+            // Act
+            DateTime actual = DateExtensions.FromUnixTime(unixTime);
+            
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestToUnixTime_MinValue()
+        {
+            // Arrange
+            DateTime dt = DateTime.MinValue;
+            long expected = 0;
+            
+            // Act
+            long actual = dt.ToUnixTime();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestToUnixTime_Positive()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2023, 3, 18, 20, 20, 0, DateTimeKind.Utc);
+            long expected = 1679170800;
+
+            // Act
+            long actual = dt.ToUnixTime();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestToUnixTime_Negative()
+        {
+            // Arrange
+            DateTime dt = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            long expected = -2208988800; // 1900-01-01 00:00:00 UTC
+            
+            // Act
+            long actual = dt.ToUnixTime();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestStartOfPreviousMonth_SameYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2023, 5, 15);
+            DateTime expected = new DateTime(2023, 4, 1);
+
+            // Act
+            DateTime actual = dt.StartOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestStartOfPreviousMonth_ChangeYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2023, 1, 10);
+            DateTime expected = new DateTime(2022, 12, 1);
+
+            // Act
+            DateTime actual = dt.StartOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestStartOfPreviousMonth_LeapYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2020, 3, 1);
+            DateTime expected = new DateTime(2020, 2, 1);
+
+            // Act
+            DateTime actual = dt.StartOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestEndOfPreviousMonth_SameYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2021, 5, 15);
+            DateTime expected = new DateTime(2021, 4, 30, 23, 59, 59, 999, DateTimeKind.Local);
+
+            // Act
+            DateTime actual = dt.EndOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected.Date, actual.Date);
+        }
+
+        [Test]
+        public void TestEndOfPreviousMonth_ChangeYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2021, 1, 10);
+            DateTime expected = new DateTime(2020, 12, 31, 23, 59, 59, 999, DateTimeKind.Local);
+
+            // Act
+            DateTime actual = dt.EndOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected.Date, actual.Date);
+        }
+
+        [Test]
+        public void TestEndOfPreviousMonth_LeapYear()
+        {
+            // Arrange
+            DateTime dt = new DateTime(2020, 3, 1);
+            DateTime expected = new DateTime(2020, 2, 29, 23, 59, 59, 999, DateTimeKind.Local);
+
+            // Act
+            DateTime actual = dt.EndOfPreviousMonth();
+
+            // Assert
+            Assert.AreEqual(expected.Date, actual.Date);
+        }
+    }
+}
