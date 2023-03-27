52b613022d40c8d7724d23aa178b7f7cc426edc4
CodeCasterNL <1726333+CodeCasterNL@users.noreply.github.com>
2023-03-27 15:13:16 +0200

    Add docs & test for Enum.GetDisplayName()

--- src/NetEnhancements.Util/EnumExtensions.cs
+++ src/NetEnhancements.Util/EnumExtensions.cs
@@ -1,21 +1,47 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace NetEnhancements.Util
 {
+    /// <summary>
+    /// Extension methods for enums.
+    /// </summary>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Read the <see cref="DisplayAttribute"/>'s <see cref="DisplayAttribute.Name"/> property from an enum member.
+        ///
+        /// When the member can't be found by name, or when none of the attributes is found, the passed enum member's <see cref="Enum.ToString()"/> will be returned.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// enum Foo
+        /// {
+        ///     [Display(Name = "Foo-1")]
+        ///     Foo1
+        /// }
+        ///
+        /// // will contain "Foo-1".
+        /// var foo1Name = Foo.Foo1.GetDisplayName();
+        /// </code>
+        /// </example>
         public static string GetDisplayName(this Enum enumMember)
         {
             var enumMemberName = enumMember.ToString();
 
-            var attribute = enumMember.GetType()
+            var member = enumMember.GetType()
                              .GetMember(enumMemberName)
-                             .FirstOrDefault()
-                             ?.GetCustomAttribute<DisplayAttribute>()
-                             ?.Name;
+                             .FirstOrDefault();
 
-            return attribute ?? enumMemberName;
+            if (member == null)
+            {
+                return enumMemberName;
+            }
+
+            var displayAttribute = member.GetCustomAttribute<DisplayAttribute>();
+
+            return displayAttribute?.Name ?? enumMemberName;
         }
     }
 }
