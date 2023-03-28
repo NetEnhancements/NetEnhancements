b96dc43476987e8137f51e83c7c6261512b755dd
CodeCasterNL <1726333+CodeCasterNL@users.noreply.github.com>
2023-03-28 11:16:45 +0200

    Add ClosedXML enhancements

---
+++ src/NetEnhancements.ClosedXML/CellExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ClosedXML.Excel;
+
+namespace NetEnhancements.ClosedXML
+{
+    /// <summary>
+    /// Extension method container.
+    /// </summary>
+    public static class CellExtensions
+    {
+        /// <summary>
+        /// Read a cell's value, returning a default value when unable to.
+        /// </summary>
+        public static TValue? GetNullableValue<TValue>(this IXLCell cell, TValue? defaultValue = default)
+            where TValue : IComparable<TValue>
+        {
+            return cell.TryGetValue(out TValue value) ? value ?? defaultValue : defaultValue;
+        }
+    }
+}
