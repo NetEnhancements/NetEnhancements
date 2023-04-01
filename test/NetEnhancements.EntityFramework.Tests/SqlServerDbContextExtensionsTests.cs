040eb684b80b5edf31ed66dd7db88c0692010edd
CodeCasterNL <1726333+CodeCasterNL@users.noreply.github.com>
2023-04-01 17:08:24 +0200

    Testing DbContext.EnableIdentityInsertAsync()

---
+++ test/NetEnhancements.EntityFramework.Tests/SqlServerDbContextExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using NetEnhancements.EntityFramework.Tests.Relational;
+
+namespace NetEnhancements.EntityFramework.Tests
+{
+    public class SqlServerDbContextExtensionsTests
+    {
+        [Test]
+        public void EnableIdentityInsertAsync()
+        {
+            // Arrange
+            var testContext = new InMemoryTestDbContext();
+
+            // Act & Assert
+            // We can't actually test what gets sent to the database without a whole lot of mocking. This is here for coverage.
+            Assert.ThrowsAsync<InvalidOperationException>(
+                async () => await testContext.EnableIdentityInsertAsync<TestEntity>(enable: true), 
+                "Relational-specific methods can only be used when the context is using a relational database provider."
+            );
+        }
+    }
+}
