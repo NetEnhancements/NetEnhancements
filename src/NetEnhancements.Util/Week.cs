f020ddf919fc1f062670e4eb331d58d9f6f3b542
CodeCasterNL <1726333+CodeCasterNL@users.noreply.github.com>
2023-06-05 19:24:59 +0200

    Add Week and Week.GetBetween(DateOnly, DateOnly)

---
+++ src/NetEnhancements.Util/Week.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace NetEnhancements.Util;
+
+/// <summary>
+/// Represents a calendar week.
+/// </summary>
+public sealed record Week(DateOnly Start, DateOnly End)
+{
+    /// <summary>
+    /// The ISO 8601 week number.
+    /// </summary>
+    public int Number => ISOWeek.GetWeekOfYear(Start.ToDateTime(default));
+
+    /// <summary>
+    /// Get the weeks between two dates.
+    /// </summary>
+    /// <param name="startDate"></param>
+    /// <param name="endDate"></param>
+    /// <param name="firstDayOfWeek"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">When the end date is before the start date.</exception>
+    public static IReadOnlyList<Week> GetBetween(DateOnly startDate, DateOnly endDate, DayOfWeek firstDayOfWeek = DayOfWeek.Monday)
+    {
+        if (endDate < startDate)
+        {
+            throw new ArgumentException("endDate must lie on or beyond startDate");
+        }
+
+        if (endDate == startDate) return new[]
+        {
+            new Week(startDate, endDate)
+        };
+
+        var lastDayOfWeek = firstDayOfWeek - 1 + 7 % 7;
+
+        var weeks = new List<Week>();
+        var weekEndDate = new[] { endDate, startDate.Next(lastDayOfWeek) }.Min();
+
+        for (
+            var weekStartDate = startDate;
+            weekStartDate < endDate;
+            weekStartDate = weekEndDate.AddDays(1),
+            weekEndDate = new[] { endDate, weekEndDate.AddDays(7) }.Min()
+        )
+        {
+            weeks.Add(new Week(weekStartDate, weekEndDate));
+        }
+
+        return weeks.ToArray();
+    }
+}
---
+++ test/NetEnhancements.Util.Tests/WeekTests.cs
@@ -0,0 +1,88 @@
+﻿namespace NetEnhancements.Util.Tests
+{
+    public class WeekTests
+    {
+        /// <summary>
+        /// Wednesday till Wednesday two weeks later = 3 weeks
+        /// </summary>
+        [Test]
+        public void WeeksBeteen_Midweek_AddsWeek()
+        {
+            // Arrange
+            var startDate = new DateOnly(2023, 05, 11);
+            var endDate = new DateOnly(2023, 05, 25);
+
+            // Act
+            var weeks = Week.GetBetween(startDate, endDate);
+
+            // Assert
+            Assert.That(weeks, Has.Count.EqualTo(3));
+            Assert.Multiple(() =>
+            {
+                Assert.That(weeks[0].Start, Is.EqualTo(startDate));
+                Assert.That(weeks[0].End, Is.EqualTo(new DateOnly(2023, 05, 14)));
+                Assert.That(weeks[1].Start, Is.EqualTo(new DateOnly(2023, 05, 15)));
+                Assert.That(weeks[1].End, Is.EqualTo(new DateOnly(2023, 05, 21)));
+                Assert.That(weeks[2].Start, Is.EqualTo(new DateOnly(2023, 05, 22)));
+                Assert.That(weeks[2].End, Is.EqualTo(endDate));
+            });
+        }
+
+        [Test]
+        public void WeeksBeteen_FullWeek_OneWeek()
+        {
+            // Arrange
+            var startDate = new DateOnly(2023, 05, 29);
+            var endDate = new DateOnly(2023, 06, 04);
+
+            // Act
+            var weeks = Week.GetBetween(startDate, endDate);
+
+            // Assert
+            Assert.That(weeks, Has.Count.EqualTo(1));
+            Assert.Multiple(() =>
+            {
+                Assert.That(weeks.First().Start, Is.EqualTo(startDate));
+                Assert.That(weeks.Last().End, Is.EqualTo(endDate));
+            });
+        }
+
+        [Test]
+        public void WeeksBeteen_SubWeek_OneWeek()
+        {
+            // Arrange
+            var startDate = new DateOnly(2023, 05, 04);
+            var endDate = new DateOnly(2023, 05, 05);
+
+            // Act
+            var weeks = Week.GetBetween(startDate, endDate);
+
+            // Assert
+            Assert.That(weeks, Has.Count.EqualTo(1));
+            Assert.Multiple(() =>
+            {
+                Assert.That(weeks.First().Start, Is.EqualTo(startDate));
+                Assert.That(weeks.Last().End, Is.EqualTo(endDate));
+            });
+        }
+
+        [Test]
+        public void WeeksBeteen_OneDay_OneWeek()
+        {
+            // Arrange
+            var startDate = new DateOnly(2023, 05, 04);
+            var endDate = new DateOnly(2023, 05, 04);
+
+            // Act
+            var weeks = Week.GetBetween(startDate, endDate);
+
+            // Assert
+            Assert.That(weeks, Has.Count.EqualTo(1));
+            Assert.Multiple(() =>
+            {
+                Assert.That(weeks.First().Start, Is.EqualTo(startDate));
+                Assert.That(weeks.Last().End, Is.EqualTo(endDate));
+            });
+        }
+    }
+}
