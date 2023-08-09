dd336922d343e0b53697e4d7fc4db55f260db64b
CodeCasterNL <1726333+CodeCasterNL@users.noreply.github.com>
2023-08-09 22:55:46 +0200

    Add [ExcelColumnAddress("A")] to indicate column for property

---
+++ src/NetEnhancements.ClosedXML/ExcelColumnAddressAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace NetEnhancements.ClosedXML
+{
+    /// <summary>
+    /// Determine in which column letter(s) the property's data resides.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExcelColumnAddressAttribute : Attribute
+    {
+        /// <summary>
+        /// The 1-based index of the column.
+        /// </summary>
+        public int Index { get; }
+
+        /// <summary>
+        /// Determines the column address ("A" = 1, "AA" = 27, ...).
+        /// </summary>
+        public ExcelColumnAddressAttribute(string address)
+        {
+            Index = ColumnExtensions.LetterToIndex(address);
+        }
+
+        /// <summary>
+        /// Determines the 1-based column's index.
+        /// </summary>
+        public ExcelColumnAddressAttribute(int index)
+        {
+            Index = index;
+        }
+    }
+}
