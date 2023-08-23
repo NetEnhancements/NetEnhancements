7628f21032c05baf57154804bc7591a5b490f48f
Maikel <skami.creations@gmail.com>
2023-08-23 20:29:39 +0200

    XML Comments for conditional styling enum

--- src/NetEnhancements.ClosedXML/ExcelColumnConditionalStyleAttribute.cs
+++ src/NetEnhancements.ClosedXML/ExcelColumnConditionalStyleAttribute.cs
@@ -11,22 +11,85 @@
         public Condition Condition { get; set; }
     }
 
+    /// <summary>
+    /// Specifies the conditions for when to apply the conditional styling.
+    /// </summary>
     public enum Condition
     {
+        /// <summary>
+        /// Apply conditional styling when the value is blank.
+        /// </summary>
         WhenIsBlank,
+
+        /// <summary>
+        /// Apply conditional styling when the value is not blank.
+        /// </summary>
         WhenNotBlank,
+
+        /// <summary>
+        /// Apply conditional styling when there's an error.
+        /// </summary>
         WhenIsError,
+
+        /// <summary>
+        /// Apply conditional styling when there's no error.
+        /// </summary>
         WhenNotError,
+
+        /// <summary>
+        /// Apply conditional styling when the value contains a specified substring.
+        /// </summary>
         WhenContains,
+
+        /// <summary>
+        /// Apply conditional styling when the value doesn't contain a specified substring.
+        /// </summary>
         WhenNotContains,
+
+        /// <summary>
+        /// Apply conditional styling when the value starts with a specified substring.
+        /// </summary>
         WhenStartsWith,
+
+        /// <summary>
+        /// Apply conditional styling when the value ends with a specified substring.
+        /// </summary>
         WhenEndsWith,
+
+        /// <summary>
+        /// Apply conditional styling when the value is equal to a specified value.
+        /// </summary>
         WhenEquals,
+
+        /// <summary>
+        /// Apply conditional styling when the value is not equal to a specified value.
+        /// </summary>
         WhenNotEquals,
+
+        /// <summary>
+        /// Apply conditional styling when the value is greater than a specified value.
+        /// </summary>
         WhenGreaterThan,
+
+        /// <summary>
+        /// Apply conditional styling when the value is less than a specified value.
+        /// </summary>
         WhenLessThan,
+
+        /// <summary>
+        /// Apply conditional styling when the value is equal to or greater than a specified value.
+        /// </summary>
         WhenEqualOrGreaterThan,
+
+        /// <summary>
+        /// Apply conditional styling when the value is equal to or less than a specified value.
+        /// </summary>
         WhenEqualOrLessThan,
+
+        /// <summary>
+        /// Apply conditional styling when the value is true.
+        /// </summary>
         WhenIsTrue
     }
+
 }
