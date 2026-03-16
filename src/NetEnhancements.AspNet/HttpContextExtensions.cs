5155a4529fccfc9cfea3353e88097b18b3dad7e8
Raymon Turkenburg <1726333+CodeCasterNL@users.noreply.github.com>
2026-03-16 13:39:56 +0100

    Remove unconditional reading of spoofable header

--- src/NetEnhancements.AspNet/HttpContextExtensions.cs
+++ src/NetEnhancements.AspNet/HttpContextExtensions.cs
@@ -5,17 +5,14 @@ namespace NetEnhancements.AspNet
 {
     public static class HttpContextExtensions
     {
+        /// <summary>
+        /// Returns the IPv4(-mapped from IPv6 when necessary) address of the client making the request. If the address is not available, returns "(Unknown)".
+        /// </summary>
         [DebuggerStepThrough]
         public static string GetRequestIPAddress(this HttpContext context)
         {
-            if (context.Request.Headers.TryGetValue(SharedConstants.XForwardedFor, out var forwardedHeader))
-            {
-                return forwardedHeader.ToString().Split(',').First();
-            }
-
             return context.Connection.RemoteIpAddress?.MapToIPv4().ToString()
                 ?? SharedConstants.UnknownIpAddress;
-
         }
     }
 }
